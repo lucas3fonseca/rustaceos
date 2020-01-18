e8d45349064360ed5dd7baa0bedec67172e6b06a
Leo Ribeiro <leordev@gmail.com>
2020-01-18 12:51:15 -0500

    wip numeric

--- eosio_cdt/src/eos/numeric.rs
+++ eosio_cdt/src/eos/numeric.rs
@@ -1,15 +1,54 @@
-use bytes::{Buf, BufMut, Bytes, BytesMut};
+use bytes::{Buf, Bytes};
+use serde::{Serialize, Deserialize, Serializer, Deserializer};
 
-pub fn push_varuint32(buf: &mut BytesMut, v: u32) {
-    let mut val: u64 = v as u64;
-    loop {
-        let mut b: u8 = (val & 0x7f) as u8;
-        val >>= 7;
-        b |= (if val > 0 { 1 } else { 0 }) << 7;
-        buf.put_u8(b);
-        if val == 0 {
-            break;
+pub struct Varuint32(u32);
+
+impl Serialize for Varuint32 {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut bytes = Vec::new();
+        let mut val: u64 = self.0 as u64;
+        loop {
+            let mut b: u8 = (val & 0x7f) as u8;
+            val >>= 7;
+            b |= (if val > 0 { 1 } else { 0 }) << 7;
+            bytes.push(b);
+            if val == 0 {
+                break;
+            }
         }
+        serializer.serialize_bytes(&bytes[..])
+    }
+}
+
+impl<'de> Deserialize<'de> for Varuint32 {
+    fn deserialize<D>(deserializer: D) -> Result<Varuint32, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        use std::mem::size_of;
+
+        let mut n = 0;
+        let mut shift = 0;
+        let mut byte: u8 = Deserialize::deserialize(&mut deserializer)?;
+
+        // Only allow reading size_of + 1 bytes to avoid overflows on bitshifts
+        for _ in 0..(size_of::<usize>()) {
+            if byte < 128 {
+                break;
+            }
+
+            n |= ((byte & 127) as usize) << shift;
+            shift += 7;
+            byte = Deserialize::deserialize(deserializer)?;
+        }
+
+        let value = n | ((byte as usize) << shift);
+        // Ok(Varuint32(value))
+        // let s = String::deserialize(deserializer)?;
+        Ok(Varuint32(value as u32))
     }
 }
 
