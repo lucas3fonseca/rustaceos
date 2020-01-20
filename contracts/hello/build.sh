15787f60a3077945554069122828f6a59628dc83
Leo Ribeiro <leordev@gmail.com>
2020-01-19 19:02:35 -0500

    fixes contracts build

--- contracts/hello/build.sh
+++ contracts/hello/build.sh
@@ -1,3 +1,3 @@
 cargo +nightly build --release --target=wasm32-unknown-unknown
-wasm-gc ../../target/wasm32-unknown-unknown/release/hello.wasm hello_gc.wasm
+wasm-gc ./target/wasm32-unknown-unknown/release/hello.wasm hello_gc.wasm
 wasm-opt hello_gc.wasm --output hello_gc_opt.wasm -Oz
