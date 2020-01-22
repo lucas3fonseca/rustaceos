6444744e19ca2367303da3d158a2b1cc8a16099d
Leo Ribeiro <leordev@gmail.com>
2020-01-22 07:59:13 -0500

    table find

--- contracts/hello/src/age.rs
+++ contracts/hello/src/age.rs
@@ -7,17 +7,42 @@ pub struct UserAge {
     age: u16,
 }
 
-impl UserAge {
-    const NAME: eos::Name = eos::name_from_str("usersage");
-    const SCOPE: eos::Name = eos::name_from_str("");
+pub struct UserAgeTable<'a> {
+    code: &'a eos::Name,
+    scope: eos::Name,
+    name: eos::Name,
+    itr: Option<i32>,
+    data: Option<&'a UserAge>,
+}
 
-    fn insert(&self, payer: &eos::Name) {
-        eosio_cdt::table_insert(&UserAge::SCOPE, &UserAge::NAME, payer, self.user.value, &self);
+impl<'a> UserAgeTable<'a> {
+    pub fn new(code: &'a eos::Name, scope: eos::Name) -> Self {
+        UserAgeTable {
+            code,
+            scope,
+            name: eos::name_from_str("usersage"),
+            itr: None,
+            data: None,
+        }
     }
 
-    pub fn find(id: u64) -> UserAge {
-        // eosio_cdt::table_find()
-        UserAge { user: eos::Name::new(1), age: 99 }
+    fn insert(&self, payer: &eos::Name) {
+        check(self.data.is_some(), "the data of the table row is empty");
+        let data = &self.data.unwrap();
+        eosio_cdt::table_insert(&self.scope, &self.name, payer, data.user.value, data);
+    }
+
+    pub fn find(&self, id: u64) {
+        let itr = Some(eosio_cdt::table_find(
+            &self.code,
+            &self.scope,
+            &self.name,
+            id,
+        ));
+    }
+
+    pub fn end(&self) -> i32 {
+        eosio_cdt::table_end(&self.code, &self.scope, &self.name)
     }
 }
 
@@ -26,11 +51,10 @@ pub fn signupage(name: eos::Name, age: u16) {
     require_auth(contract.get_self());
     check_age(age);
 
-    let user_age = UserAge {
-        user: name,
-        age,
-    };
-    user_age.insert(contract.get_self());
+    let user_age = UserAge { user: name, age };
+    let mut user_age_table = UserAgeTable::new(contract.get_self(), eos::Name::new(0));
+    user_age_table.data = Some(&user_age);
+    user_age_table.insert(contract.get_self());
 
     accepted_message(user_age.user, age);
 }
@@ -40,7 +64,7 @@ pub fn updateage(name: eos::Name, age: u16) {
     require_auth(contract.get_self());
     check_age(age);
 
-    let user_age = UserAge::find(name.value);
+    // let user_age = UserAge::find(name.value);
     // user_age.age = age;
     // user_age.update(contract.get_self());
 
@@ -56,7 +80,7 @@ fn check_age(age: u16) {
 
 fn accepted_message(name: eos::Name, age: u16) {
     print!(
-        "Hey, it looks like the user ", name, " is ",
-        age, " years old. He/she is considered adult for our services."
+        "Hey, it looks like the user ",
+        name, " is ", age, " years old. He/she is considered adult for our services."
     );
 }
--- eosio_cdt/src/cdt/table.rs
+++ eosio_cdt/src/cdt/table.rs
@@ -3,6 +3,14 @@ use serde::Serialize;
 use crate::eos;
 use crate::eosio_cdt_bindings;
 
+pub fn table_find(code: &eos::Name, scope: &eos::Name, table: &eos::Name, id: u64) -> i32 {
+    unsafe { eosio_cdt_bindings::db_find_i64(code.value, scope.value, table.value, id) }
+}
+
+pub fn table_end(code: &eos::Name, scope: &eos::Name, table: &eos::Name) -> i32 {
+    unsafe { eosio_cdt_bindings::db_end_i64(code.value, scope.value, table.value) }
+}
+
 pub fn table_insert<T: ?Sized>(
     scope: &eos::Name,
     table: &eos::Name,
