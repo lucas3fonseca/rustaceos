121793e2badac34fefb47322cdbae3929c08053c
Leo Ribeiro <leordev@gmail.com>
2020-01-27 22:13:09 -0500

    uses wasm-logger

--- dapp-explorer/src/lib.rs
+++ dapp-explorer/src/lib.rs
@@ -17,7 +17,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen]
 pub fn run_app() -> Result<(), JsValue> {
     utils::set_panic_hook();
-    utils::logger::init();
+    wasm_logger::init(wasm_logger::Config::default());
     yew::start_app::<app::App>();
     Ok(())
 }
